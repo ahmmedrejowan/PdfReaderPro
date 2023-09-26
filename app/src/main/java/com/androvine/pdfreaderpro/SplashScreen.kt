8def4db39f6a3b862fc122d904f437c3fb7e2211
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-09-26 23:36:41 +0600

    Initial Commit

---
+++ app/src/main/java/com/androvine/pdfreaderpro/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.androvine.pdfreaderpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
