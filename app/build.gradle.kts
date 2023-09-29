beecd92450be534c3cd01ffa25034c149e81d91c
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-09-29 18:09:17 +0600

    Intro Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // lottie animation
+    implementation("com.airbnb.android:lottie:5.2.0")
+
 }
