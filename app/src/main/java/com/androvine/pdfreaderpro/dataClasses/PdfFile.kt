7f2d33dc14b4f58b0072945a54c1bce298a39059
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-10-05 21:19:16 +0600

    Sorting ignore case

--- app/src/main/java/com/androvine/pdfreaderpro/dataClasses/PdfFile.kt
+++ app/src/main/java/com/androvine/pdfreaderpro/dataClasses/PdfFile.kt
@@ -6,10 +6,9 @@ data class PdfFile(
     val size: Long,
     val dateModified: Long,
     val parentFolderName: String
-
 ) {
     companion object {
-        val SortByName = Comparator<PdfFile> { o1, o2 -> o1.name.compareTo(o2.name) }
+        val SortByName = Comparator<PdfFile> { o1, o2 -> o1.name.compareTo(o2.name, true) }
         val SortByDate =
             Comparator<PdfFile> { o1, o2 -> o2.dateModified.compareTo(o1.dateModified) }
         val SortBySize = Comparator<PdfFile> { o1, o2 -> o2.size.compareTo(o1.size) }
--- app/src/main/java/com/androvine/pdfreaderpro/dataClasses/PdfFolder.kt
+++ app/src/main/java/com/androvine/pdfreaderpro/dataClasses/PdfFolder.kt
@@ -5,7 +5,7 @@ data class PdfFolder(
     val pdfFiles: List<PdfFile>
 ) {
     companion object {
-        val SortByName = Comparator<PdfFolder> { o1, o2 -> o1.name.compareTo(o2.name) }
+        val SortByName = Comparator<PdfFolder> { o1, o2 -> o1.name.compareTo(o2.name, true) }
         val SortBySize = Comparator<PdfFolder> { o1, o2 -> o2.pdfFiles.size.compareTo(o1.pdfFiles.size) }
 
     }
