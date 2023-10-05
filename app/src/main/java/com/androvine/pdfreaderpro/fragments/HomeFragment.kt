ec6359c5c04947314485bf912bf2cdee8d0117c3
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-10-05 10:50:55 +0600

    Home Fragment

--- app/src/main/java/com/androvine/pdfreaderpro/fragments/HomeFragment.kt
+++ app/src/main/java/com/androvine/pdfreaderpro/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.androvine.pdfreaderpro.fragments
 
 import android.os.Bundle
+import android.text.format.Formatter.formatFileSize
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -36,15 +37,35 @@ class HomeFragment : Fragment() {
 
         pdfListViewModel.pdfFiles.observe(viewLifecycleOwner) { pdfFiles ->
 
-            Log.e("HomeFragment", "onViewCreated: total pdf files: ${pdfFiles.size}")
+            binding.loadingFileCount.visibility = View.GONE
+            binding.storageLayout.visibility = View.VISIBLE
+            binding.totalFiles.visibility = View.VISIBLE
+            binding.totalFilesTitle.visibility = View.VISIBLE
 
+            binding.totalFiles.text = pdfFiles.size.toString()
+
+            val totalFileSize = formatFileSize(requireContext(), pdfFiles.sumOf { it.size })
+            binding.pdfSize.text = totalFileSize.substring(0, totalFileSize.length - 2)
+            binding.pdfSizeUnit.text = totalFileSize.substring(totalFileSize.length - 2)
+
+            // get total device storage size
+            val file = android.os.Environment.getDataDirectory()
+            val stat = android.os.StatFs(file.path)
+            val blockSize = stat.blockSizeLong
+            val totalBlocks = stat.blockCountLong
+            val total = totalBlocks * blockSize
+            val totalFormatted = formatFileSize(requireContext(), total)
+            binding.totalSize.text = totalFormatted.substring(0, totalFormatted.length - 2)
+            binding.totalSizeUnit.text = totalFormatted.substring(totalFormatted.length - 2)
         }
 
     }
 
     private fun setUpInitialView() {
-
-
+        binding.storageLayout.visibility = View.GONE
+        binding.loadingFileCount.visibility = View.VISIBLE
+        binding.totalFiles.visibility = View.GONE
+        binding.totalFilesTitle.visibility = View.GONE
     }
 
 
