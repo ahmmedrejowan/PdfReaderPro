7fe32007dc7397a1f96fe8819a7ce9ab68422967
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-10-07 02:26:57 +0600

    cleanup

--- app/src/main/java/com/androvine/pdfreaderpro/activities/Home.kt
+++ app/src/main/java/com/androvine/pdfreaderpro/activities/Home.kt
@@ -1,11 +1,7 @@
 package com.androvine.pdfreaderpro.activities
 
 import android.content.Intent
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -100,22 +96,6 @@ class Home : AppCompatActivity() {
         })
 
 
-    }
-
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is EditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
-                }
-            }
-        }
-        return super.dispatchTouchEvent(event)
     }
 
 
